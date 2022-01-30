3f4b1965ebb9a84c9679c9543f78085361cbe60e
iamvigneshwars <iamvigneshwars@gmail.com>
2022-01-30 00:04:19 +0000

    R
    Random Play complete.

--- static/js/script.js
+++ static/js/script.js
@@ -12,20 +12,20 @@ function sleep(ms) {
   }
 
 
-function decideWinner(user_choise, ai_choise){
+function decideWinner(user_choice, ai_choice){
     // TIE situation
-    if (user_choise == ai_choise) return 0;
+    if (user_choice == ai_choice) return 0;
 
     // Player win situations
-    if (user_choise == "rock" && ai_choise == "scissors") return 1;
-    if (user_choise == "paper" && ai_choise == "rock") return 1;
-    if (user_choise == "scissors" && ai_choise == "paper") return 1;
+    if (user_choice == "rock" && ai_choice == "scissors") return 1;
+    if (user_choice == "paper" && ai_choice == "rock") return 1;
+    if (user_choice == "scissors" && ai_choice == "paper") return 1;
 
     // Player lose situations
 
-    if (user_choise == "scissors" && ai_choise == "rock") return 2;
-    if (user_choise == "rock" && ai_choise == "paper") return 2;
-    if (user_choise == "paper" && ai_choise == "scissors") return 2;
+    if (user_choice == "scissors" && ai_choice == "rock") return 2;
+    if (user_choice == "rock" && ai_choice == "paper") return 2;
+    if (user_choice == "paper" && ai_choice == "scissors") return 2;
 
 }
 
@@ -33,22 +33,22 @@ function decideWinner(user_choise, ai_choise){
 function rpsGame(userInput){
 
     // Choises that the user and AI can make
-    var choises = ["scissors", "rock", "paper"];
-    // Get the user choise from the button clicked
-    var user_choise = document.getElementById(userInput.id).id;
+    var choices = ["scissors", "rock", "paper"];
+    // Get the user choice from the button clicked
+    var user_choice = document.getElementById(userInput.id).id;
 
     var decision = ["TIE", "WIN", "LOSE"];
-    // Generate a random choise for the AI 
+    // Generate a random choice for the AI 
     const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-    var ai_choise = choises[random(0, 3)];
+    var ai_choice = choices[random(0, 3)];
 
-    // Decide the winner based on the choises (0: TIE, 1: WIN, 2: LOSE)
-    var winner = decideWinner(user_choise, ai_choise)
+    // Decide the winner based on the choices (0: TIE, 1: WIN, 2: LOSE)
+    var winner = decideWinner(user_choice, ai_choice)
     // Increament the score with corresponding result
     SCORES[decision[winner]]++;
 
 
-    console.log(user_choise, ai_choise)
+    console.log(user_choice, ai_choice)
     console.log(decision[winner]);
     console.log("Score: ", decision[winner], SCORES[decision[winner]]);
 
@@ -65,9 +65,12 @@ function rpsGame(userInput){
 
     // Update Player image
     document.getElementById("user_play").src = "static/images/rest.png"
-    sleep(100).then(() => { document.getElementById("user_play").src = "static/images/" + user_choise + ".png"; });
+    sleep(100).then(() => { document.getElementById("user_play").src = "static/images/" + user_choice + ".png"; });
 
     // Update AI image
     document.getElementById("ai_play").src = "static/images/rest_ai.png";
-    sleep(100).then(() => { document.getElementById("ai_play").src = "static/images/" + ai_choise +"_ai" + ".png"; });
+    sleep(100).then(() => { document.getElementById("ai_play").src = "static/images/" + ai_choice +"_ai" + ".png"; });
+
+    // Update the DOGE emoji
+    document.getElementById("doge").src = "static/images/doge_" + decision[winner]  + ".png"
 }
